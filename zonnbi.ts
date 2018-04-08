2e01100f9c6c7bbd9705d53b65caf32488fcb511
gomame07 <mikko.nari@gmail.com>
2018-04-08 09:50:16 +0000

    Update zonnbi.ts

--- zonnbi.ts
+++ zonnbi.ts
@@ -4,7 +4,7 @@ function zombieA(){
 
     const sender = magik.getSender();
     const EntityType = magik.type("entity.EntityType");
-    const Zombie = EntityType["Zombie"];
+    const Zombie = EntityType['Zombie'];
 
     for (var i = 0; i < 100; i++){
         var arrow = sender.getworld().spawnEntity(magik.aspecto().zonbie);
