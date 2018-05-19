57582a99c79daaeaa69390accfbae605e4002809
gomame07 <mikko.nari@gmail.com>
2018-05-19 23:46:57 +0000

    Update ennda.ts

---
+++ ennda.ts
@@ -0,0 +1,12 @@
+const magik = magikcraft.io;
+function ennda(){
+
+    const sender = magik.getSender();
+    const EntityType = magik.type("entity.EntityType");
+    const ennda = EntityType['['ENDERMAN'];'];
+
+    for (var i = 0; i < 100; i++) {
+        var arrow = sender.getWorld().spawnEntity(magik.aspecto(), Golem);
+    }
+
+}
